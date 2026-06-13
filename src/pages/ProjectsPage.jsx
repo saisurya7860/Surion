426e05a0b7da47b8a886fba9ece7f89451ab06d1
saisurya7860 <saisurya7860@gmail.com>
2026-06-13 22:25:33 +0530

    added loader to project page

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -4,36 +4,49 @@ import ProjectCard from "../components/projects/ProjectCard";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import Header1 from "../layouts/Header1";
+import Loader from "../components/ui/Loader";
 
 const ProjectsPage = () => {
   const [projects, setProjects] = useState([]);
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
     const fetch = async () => {
-      const res = await getProjects();
-      console.log(res);
-      setProjects(res.data);
+      try {
+        setLoading(true);
+        const res = await getProjects();
+        console.log(res);
+        setProjects(res.data);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
     };
     fetch();
   }, []);
 
   return (
     <>
-      <div className="max-w-7xl mx-auto px-4 py-10">
-        <div className="pb-15">
-          <div className="text-center text-4xl font-bold">All Projects</div>
-          <div className="text-center text-xl text-gray-500 mt-2">
-            Explore all the projects I've worked on. Filter or search to find
-            something specific.
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className="max-w-7xl mx-auto px-4 py-10">
+          <div className="pb-15">
+            <div className="text-center text-4xl font-bold">All Projects</div>
+            <div className="text-center text-xl text-gray-500 mt-2">
+              Explore all the projects I've worked on. Filter or search to find
+              something specific.
+            </div>
+          </div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+            {projects.map((project) => (
+              <ProjectCard key={project.id} project={project} />
+            ))}
           </div>
         </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-          {projects.map((project) => (
-            <ProjectCard key={project.id} project={project} />
-          ))}
-        </div>
-      </div>
+      )}
     </>
   );
 };
